866d31e9954457d11606117346a0268c484e5ab4
Santiago Pereira <santiagope.sp@gmail.com>
2025-02-25 00:46:29 -0300

    Creacion del repo.

---
+++ animaciones_inicio.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded' , function() {
+
+    const navLink = document.querySelectorAll('nav .derecha a ') ; 
+
+    navLink.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add('active') ;
+        }
+    }) ; 
+}) ; 
+
+
